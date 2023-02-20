ce93acf13eea829f415d53658381d1695fc1126b
adey-px <aosteph@gmail.com>
2023-02-19 21:40:33 -0600

    Added data for dynamic class

--- src/components/ActionBtn.vue
+++ src/components/ActionBtn.vue
@@ -1,25 +1,41 @@
 <!-- template section -->
 <template>
-  <button
-    @click="clickHandler"
-    class="rounded border-0 bg-brand-blue-1 px-3 py-3 font-medium text-white hover:shadow-blue"
-  >
-    Sign In
-  </button>
+	<button
+		@click="clickHandler"
+		:class="{ primary }"
+	>
+		{{ label }}
+	</button>
 </template>
 
 <!-- script section -->
 <script>
-  export default {
-    name: 'ActionBtn',
-    methods: {
-      clickHandler(event) {
-        console.log(event);
-      },
-    },
-  };
+	export default {
+		name: 'ActionBtn',
+		props: ['label'],
+		data() {
+			return {
+				primary: true,
+			};
+		},
+		methods: {
+			clickHandler(event) {
+				console.log(event);
+			},
+		},
+	};
 </script>
 
 <!-- 
-  NB: type von shortcuts gives onClick (@click)
+Type 'von' shortcut gives onClick (@click)
 -->
+
+<style scoped>
+	button {
+		@apply rounded px-5 py-3 font-medium;
+	}
+
+	.primary {
+		@apply border-0 bg-brand-blue-1 text-white hover:shadow-blue;
+	}
+</style>
--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -2,76 +2,79 @@
 
 <!-- template section -->
 <template>
-  <header class="w-full text-sm">
-    <div class="fixed top-0 left-0 h-16 w-full bg-white">
-      <div
-        class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8"
-      >
-        <a
-          :href="url"
-          class="flex h-full items-center text-xl"
-        >
-          {{ logo }}
-        </a>
-        <nav class="ml-12 h-full">
-          <ul class="flex h-full list-none">
-            <li
-              v-for="navLabel in navLabels"
-              :key="navLabel"
-              class="ml-9 h-full first:ml-0"
-            >
-              <a
-                href=""
-                class="flex h-full items-center py-2.5"
-              >
-                {{ navLabel }}
-              </a>
-            </li>
-          </ul>
-        </nav>
-        <div class="ml-auto flex h-full items-center">
-          <ProfileImg v-if="isLoggedIn" />
-          <ActionBtn v-else @click="authUser" />
-        </div>
-      </div>
-    </div>
-  </header>
+	<header class="w-full text-sm">
+		<div class="fixed top-0 left-0 h-16 w-full bg-white">
+			<div
+				class="mx-auto flex h-full flex-nowrap border-b border-solid border-brand-gray-1 px-8"
+			>
+				<a
+					:href="url"
+					class="flex h-full items-center text-xl"
+				>
+					{{ logo }}
+				</a>
+				<nav class="ml-12 h-full">
+					<ul class="flex h-full list-none">
+						<li
+							v-for="navLabel in navLabels"
+							:key="navLabel"
+							class="ml-9 h-full first:ml-0"
+						>
+							<a
+								href=""
+								class="flex h-full items-center py-2.5"
+							>
+								{{ navLabel }}
+							</a>
+						</li>
+					</ul>
+				</nav>
+				<div class="ml-auto flex h-full items-center">
+					<ProfileImg v-if="isLoggedIn" />
+					<ActionBtn
+						label="Sign In"
+						v-else
+						@click="authUser"
+					/>
+				</div>
+			</div>
+		</div>
+	</header>
 </template>
 
 <!-- script section -->
 <script>
-  import ActionBtn from './ActionBtn.vue';
-  import ProfileImg from './ProfileImg.vue';
+	import ActionBtn from './ActionBtn.vue';
+	import ProfileImg from './ProfileImg.vue';
 
-  export default {
-    name: 'MainNav',
-    components: {
-      ActionBtn,
-      ProfileImg,
-    },
-    data() {
-      return {
-        logo: 'Google Careers',
-        url: 'https://careers.google.com',
-        navLabels: [
-          'Teams',
-          'Locations',
-          'Life At Google',
-          'How We Hire',
-          'Students',
-          'Jobs',
-        ],
-        isLoggedIn: false,
-      };
-    },
-    methods: {
-      authUser() {
-        this.isLoggedIn = true;
-      },
-    },
-  };
+	export default {
+		name: 'MainNav',
+		components: {
+			ActionBtn,
+			ProfileImg,
+		},
+		data() {
+			return {
+				logo: 'Google Careers',
+				url: 'https://careers.google.com',
+				navLabels: [
+					'Teams',
+					'Locations',
+					'Life At Google',
+					'How We Hire',
+					'Students',
+					'Jobs',
+				],
+				isLoggedIn: false,
+			};
+		},
+		methods: {
+			authUser() {
+				this.isLoggedIn = true;
+			},
+		},
+	};
 </script>
-
 
 <!-- methods: {} is used to update state
   of items in data() {} using this keyword.
