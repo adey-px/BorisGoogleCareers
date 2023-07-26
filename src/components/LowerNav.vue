f50169204a3d5d258da12536456e93037eb20520
adey-px <aosteph@gmail.com>
2023-07-26 14:11:28 -0500

    Icons: added font awesome icons

--- src/components/LowerNav.vue
+++ src/components/LowerNav.vue
@@ -1,6 +1,14 @@
 <!-- Template section -->
 <template>
-	<div>This is the LowerNav</div>
+	<div class="h-16 w-full border-b border-solid border-brand-gray-1 bg-white">
+		<div class="flex h-full items-center px-8">
+			<font-awesome
+				:icon="['fa-solid', 'search']"
+				class="mr-3"
+			/>
+			<span><span class="text-brand-green-1">1660</span> jobs matched</span>
+		</div>
+	</div>
 </template>
 
 <!-- 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue';
-import '@/main.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import App from './App.vue';
+import '@/globals.css';
 
-createApp(App).mount('#app');
+// Add font awesome icon needed to library
+library.add(faSearch);
+
+// Set font awesome as component to Vue App
+createApp(App).component('font-awesome', FontAwesomeIcon).mount('#app');
